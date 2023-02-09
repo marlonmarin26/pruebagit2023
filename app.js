7f7684f8e921a1e73f94470a72acd048a3ed15e4
Marlon Marín <marlon.1701910281@ucaldas.edu.co>
2023-02-09 09:35:18 -0500

    Se agrega la función de sumar en el app.js

--- app.js
+++ app.js
@@ -15,4 +15,18 @@ function restar(num1, num2) {
     return resultado;
 }
 
+/**
+ * Sumar (num1, num2) toma dos números como argumentos y devuelve el resultado de sumar el segundo
+ * numero desde el primero
+ * @param num1 - El primer número a restar
+ * @param num2 - número
+ * @returns El resultado de la operación
+ */
+function sumar(num1, num2) {
+    let resultado = num1 + num2;
+    console.log(`El resultado de la suma es ${resultado}`);
+    return resultado;
+}
+
 restar (n1, n2);
+sumar (n1, n2);
